7e259f62c2673fa5f908df36983a9090aa5da9f8
franciscot <francisco.tapia.t@gmail.com>
2021-03-14 21:32:07 -0300

    Corregido fallback al fallar Plop.js y agregado plop como comando en package.json

--- src/plop/generators/model.actions.js
+++ src/plop/generators/model.actions.js
@@ -4,11 +4,13 @@ module.exports = {
     actions: [
         {
             type: 'add',
+            abortOnFail: true,
             path: 'src/models/store/{{ dashCase name }}s/{{ pascalCase name }}MutationType.ts',
             templateFile: pathTemplates + '/store-model/MutationType.ts.hbs'
         },
         {
             type: 'add',
+            abortOnFail: true,
             path: 'src/models/store/{{ dashCase name }}s/{{ pascalCase name }}sState.interface.ts',
             templateFile: pathTemplates + '/store-model/State.interface.ts.hbs'
         },
--- src/plop/generators/module.actions.js
+++ src/plop/generators/module.actions.js
@@ -4,16 +4,19 @@ module.exports = {
     actions: [
         {
             type: 'add',
+            abortOnFail: true,
             path: 'src/store/{{ dashCase name }}s/index.ts',
             templateFile: pathTemplates + '/store-module/index.ts.hbs'
         },
         {
             type: 'add',
+            abortOnFail: true,
             path: 'src/store/{{ dashCase name }}s/InitialState.ts',
             templateFile: pathTemplates + '/store-module/InitialState.ts.hbs'
         },
         {
             type: 'add',
+            abortOnFail: true,
             path: 'src/store/{{ dashCase name }}s/module.ts',
             templateFile: pathTemplates + '/store-module/module.ts.hbs'
         },
