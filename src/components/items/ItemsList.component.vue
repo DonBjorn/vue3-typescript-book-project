09fbe650bb5b2d7429cbb56be152c7a6bddbf105
Francisco Tapia <francisco.tapia.t@gmail.com>
2022-07-19 12:39:13 -0400

    Capitulo 12. Página 159

--- src/components/items/ItemsList.component.vue
+++ src/components/items/ItemsList.component.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>Items - loading : {{ loading }} </h3>
+    <h3>{{ i18n.t('items.list.header') }}:</h3>
     <Loader v-show="loading"></Loader>
     <ul v-show="!loading">
       <ItemComponent :model="item" v-for="item in items" :key="item.id" @select="onItemSelect"></ItemComponent>
@@ -14,21 +14,27 @@ import {defineComponent, PropType} from 'vue';
 import {ItemInterface} from "@/models/items/Item.interface";
 import ItemComponent from "@/components/items/children/Item.component.vue";
 import Loader from "@/components/shared/Loader.component.vue";
+import {useI18n} from "vue-i18n";
 
 export default defineComponent({
   name: 'ItemsListComponent',
   components: {ItemComponent, Loader},
   props: {
-    items:  Array as PropType<ItemInterface[]>,
+    items: Array as PropType<ItemInterface[]>,
     loading: Boolean,
   },
   emits: ['selectItem'],
   setup(props, {emit}) {
+    const i18n = useI18n();
+
     const onItemSelect = (item: ItemInterface) => {
       emit('selectItem', item);
     }
 
-    return {onItemSelect};
+    return {
+      i18n,
+      onItemSelect
+    };
   }
 })
 </script>
--- src/plugins/vue-i18n-next-plugin/index.ts
+++ src/plugins/vue-i18n-next-plugin/index.ts
@@ -1,25 +1,43 @@
 import {createI18n, LocaleMessages, VueMessageType} from 'vue-i18n';
+import {match} from "sinon";
 
 interface LocalesDataInterface {
     messages: LocaleMessages<VueMessageType>
 }
 
-const data: LocalesDataInterface = {
-    messages: {
-        'en-US': {
-            welcome: 'Welcome: this message is localized in English'
-        },
-        'it-IT': {
-            welcome: 'Benvenuti: this message is localized in Italian'
-        },
-        'fr-FR': {
-            welcome: 'Bienvenue: this message is localized in French'
-        },
-        'es-ES': {
-            welcome: 'Bienvenido: this message is localized in Spanish'
-        },
+/**
+ * @name getLocalesData
+ * @description "Helper" que cargará los archivos JSON de localización
+ */
+export function getLocalesData(): LocalesDataInterface {
+    // usa require.context para cargar todos los archivos JSON de localización
+    const files = (require as any).context('./locales', true, /[A-Za-z\d-_,\s]+\.json$/);
+
+    // crea la instancia que contrndrá los datos cargados
+    const localesData: LocalesDataInterface = {
+        messages: {}
     }
+
+    // itera sobre todos los archivos
+    const keys: string[] = files.keys();
+    keys.forEach((key: string) => {
+        // extrae el nombre del archivo sin extensión
+        const matched = key.match(/([A-Za-z\d-_]+)\./i);
+
+        console.log("Matched", matched);
+
+        if (matched && matched.length > 1) {
+            const localeId = matched[1];
+            // Por cada archivo, guarda los mensajes en la propiedad de messages correspondiente
+            localesData.messages[localeId] = files(key).messages;
+        }
+    });
+
+    return localesData;
 }
+
+// Carga los datos dinamicamente
+const data: LocalesDataInterface = getLocalesData();
 
 export const i18n = createI18n({
     locale: 'es-ES',
