329d5b3ed9569656d3d7e3a9794e4fbc20d1636f
Francisco <francisco.tapia.t@gmail.com>
2022-09-04 22:43:29 -0300

    Capitulo 15, Página 194

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
         :availableLocales="availableLocales"
         @clicked="onLocaleClicked"
     />
+    <ThemeSelectorComponent />
     <div id="nav">
       <router-link to="/">{{ i18n.t('navigation.home') }}</router-link> |
       <router-link to="/about">{{ i18n.t('navigation.about') }}</router-link>
@@ -21,11 +22,11 @@ import {MutationType} from "@/models/store";
 import {useLocalesStore} from "@/store/locales";
 import {LocaleInfoInterface} from "@/models/localization/LocaleInfo.interface";
 import LocaleSelectorComponent from "@/components/locale-selector/LocaleSelector.component.vue";
+import ThemeSelectorComponent from "@/components/theme-selector/ThemeSelector.component.vue";
 
 export default defineComponent({
   name: 'App',
-  components: {LocaleSelectorComponent},
-
+  components: {LocaleSelectorComponent, ThemeSelectorComponent},
   setup(){
     const i18n = useI18n();
     const localesStore = useLocalesStore();
@@ -51,30 +52,3 @@ export default defineComponent({
 
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  padding: 10px;
-  color: #2c3e50;
-  h2 {
-    margin: 0;
-  }
-}
-
-.long-date{
-  font-size: 12px;
-}
-
-#nav {
-  padding: 8px 0;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import {rootStore} from './store'
 import {i18n} from "@/plugins/vue-i18n-next-plugin";
+import {MyAppScss} from "@/plugins/myapp-scss";
+import {FlagIconsScss} from "@/plugins/flags-icons";
 import "@/assets/scss/flag-icons/index.scss";
 
 createApp(App)
     .use(rootStore)
     .use(router)
     .use(i18n)
+    .use(MyAppScss)
+    .use(FlagIconsScss)
     .mount('#app')
